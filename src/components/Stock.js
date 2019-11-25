fdb428fb1dac590541f8d716f717c0816476f83d
Joellehelm <joellenhelm@gmail.com>
2019-11-25 14:53:20 -0600

    Done.

--- src/components/Stock.js
+++ src/components/Stock.js
@@ -1,15 +1,18 @@
 import React from 'react'
 
-const Stock = () => (
+const Stock = (props) => (
+
+  
+
   <div>
 
-    <div className="card">
+    <div onClick={() => props.stockInfo.owned ? props.sellStock(props.stockInfo) : props.buyStock(props.stockInfo)} className="card">
       <div className="card-body">
         <h5 className="card-title">{
-            //Company Name
+            props.stockInfo.name
           }</h5>
         <p className="card-text">{
-            //ticker: stock price
+            props.stockInfo.ticker + ": " + props.stockInfo.price
           }</p>
       </div>
     </div>
--- src/containers/StockContainer.js
+++ src/containers/StockContainer.js
@@ -2,13 +2,16 @@ import React, { Component } from 'react';
 import Stock from '../components/Stock'
 
 class StockContainer extends Component {
+  constructor(){
+    super()
+  }
 
   render() {
     return (
       <div>
         <h2>Stocks</h2>
         {
-          //render the list of stocks here
+          this.props.stocks.map((stock, idx) =>  <Stock buyStock={this.props.buyStock} stockInfo={stock} key={idx} />)
         }
       </div>
     );
